6f7de57a52042bf7686a173da40820fa9e253c1b
Kevin Palermo <kevpalermo@gmail.com>
2020-10-07 20:10:37 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,7 +12,6 @@ function init() {
   "b",
   "a"
 ];
-<<<<<<< HEAD
 
 function init() {
  let i = 0;
@@ -33,18 +32,3 @@ function init() {
  }
  )
  }
-=======
- let i = 0;
- if (e.key === codes[i]){
-   i++;
- } if (i === codes.length) {
-   alert("INVINCIBLE!");
-   i = 0;
- } else {
-   i = 0;
- }
-  });
-}
-
-init();
->>>>>>> dbf98b5cce14ef07d4a0b210e6c4a1b6fd117af7
